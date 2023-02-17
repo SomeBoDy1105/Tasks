9e3ba2fcfa9706b95e43ddc4bb8d96f29a85225e
SomeBoDy1105 <moustafa20035@gmail.com>
2023-02-17 19:26:08 +0100

    Added dependencies

--- flutter_task2/lib/main.dart
+++ flutter_task2/lib/main.dart
@@ -1,20 +1,15 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_task2/home_screen.dart';
+import 'package:flutter_task2/splash_screen.dart';
 void main() {
-  runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
-  }
+  runApp(
+    MaterialApp(
+      initialRoute: '/splashScreen',
+      debugShowCheckedModeBanner: false,
+      routes: {
+        '/homeScreen': (context) => const HomeScreen(),
+        '/splashScreen': (context) => const SplashScreen(),
+      },
+    ),
+  );
 }
